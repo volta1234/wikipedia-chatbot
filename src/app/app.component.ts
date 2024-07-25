32c270e644adda763d5fd0bcc93c4c8737218363
Volta1234 <calvince.ogwel@gmail.com>
2024-07-25 13:57:21 +0300

    created the add profile and edit profile templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-
-  imports: [FormsModule, RouterOutlet, RouterModule, AppComponent, CommonModule, ReactiveFormsModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+  
 })
-
 export class AppComponent {
   title = 'wikipedia-chatbot';
 }
